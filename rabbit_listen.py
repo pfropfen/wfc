59966ec31d4ba323bd4c31123937953e8555f600
LazyLoki <lokigodofbattle@gmail.com>
2025-06-05 17:10:11 +0200

    fixed port in rabbit_listen.py

--- rabbit_listen.py
+++ rabbit_listen.py
@@ -4,7 +4,7 @@ def callback(ch, method, properties, body):
     print("Message:", body)
     # ch.basic_ack(delivery_tag=method.delivery_tag)  # Don't call this if you want to requeue
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='139.6.65.27', port=31673))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='139.6.65.27', port=31672))
 channel = connection.channel()
 
 channel.basic_consume(queue='your_queue_name', on_message_callback=callback, auto_ack=False)
