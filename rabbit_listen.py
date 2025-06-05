6c065a8840e1c1fafac6b34e372dffe66aa6ee98
LazyLoki <lokigodofbattle@gmail.com>
2025-06-05 16:40:03 +0200

    rabbit listen script

---
+++ rabbit_listen.py
@@ -0,0 +1,11 @@
+import pika
+
+def callback(ch, method, properties, body):
+    print("Message:", body)
+    # ch.basic_ack(delivery_tag=method.delivery_tag)  # Don't call this if you want to requeue
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.basic_consume(queue='your_queue_name', on_message_callback=callback, auto_ack=False)
+channel.start_consuming()
